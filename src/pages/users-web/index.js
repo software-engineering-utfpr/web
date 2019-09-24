0ef2842a3b8534a9cf3ce6aa201f53e35c8492bc
Lucca-Santana <lucasrocha.2017@alunos.utfpr.edu.br>
2019-09-24 17:18:33 -0300

    some changes

--- src/pages/users-web/index.js
+++ src/pages/users-web/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Typography, Button } from 'antd';
+import { Typography, Button, Row, Col, Modal } from 'antd';
 
 import MainLayout from '../../components/layout';
 
@@ -8,10 +8,44 @@ import './style.css';
 const { Text } = Typography;
 
 const UsersWeb = props => {
+
+  const[modalCadastro, setModalCadastro] = useState(false);
+  
+  const showCadastroModal = () => {
+    setModalCadastro(true);
+  };
+
+  const handleOk = e => {
+    // console.log(e);
+    setModalCadastro(false);
+  };
+
+  const handleCancel = e => {
+    // console.log(e);
+    setModalCadastro(false);
+  };
+
   return (
     <MainLayout page = "web">
-      <Text>Users_Web</Text>
-      <Button type = "primary" style = {{ color: "white"}}> New User </Button>
+      <Row>
+        <Col span = {4} offset = {20}>
+          <Button
+            type = "primary"
+            style = {{ color: "white"}}
+            onClick = {showCadastroModal}
+          > Adicionar Usuário </Button>
+        </Col>
+      </Row>
+      <Modal
+          title="Basic Modal"
+          visible={modalCadastro}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
     </MainLayout>
   );
 };
