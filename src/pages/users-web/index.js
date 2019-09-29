3b95996748d37f2d7c6f44966450b6f4fc453180
Lucca-Santana <lucasrocha.2017@alunos.utfpr.edu.br>
2019-09-29 12:00:43 -0300

    #22 #23 web CRUD adm and fields validation

--- src/pages/users-web/index.js
+++ src/pages/users-web/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Row, Col, Modal, Input, Form, Icon, notification } from 'antd';
+import { Typography, Button, Row, Col, Modal, Input, Form, Icon, List, Avatar, Badge, Popover, Card } from 'antd';
+import { err503, err401, errGeneral, success } from '../../services/messages';
+
 // import { Redirect } from 'react-router-dom';
 
 import axios from 'axios';
@@ -8,23 +10,37 @@ import MainLayout from '../../components/layout';
 
 import './style.css';
 
-// const { Text } = Typography;
+const { Search } = Input;
+
+const { Text } = Typography;
 
 const UsersWeb = props => {
 
-  const { getFieldDecorator } = props.form;
+  const { getFieldDecorator, setFieldsValue, getFieldValue } = props.form;
 
   const[modalCadastro, setModalCadastro] = useState(false);
+  const[modalUpdate, setModalUpdate] = useState(false);
   const[managers, setManagers] = useState([{}]);
+  const[managersFiltered, setManagersFiltered] = useState([{}])
   const[confirmLoading, setConfirmLoading] = useState(false);
+  const[loadingPage, setLoadingPage] = useState(true);
+  const[itemUpdate, setItemUpdate] = useState({});
+  const[pageUpdate, setPageUpdate] = useState(false);
 
   useEffect(() => {
+    setLoadingPage(true);
     axios.get('/api/managers/').then(res => {
       // const Users = res.data;
       setManagers(res.data);
+      setManagersFiltered(res.data);
+      setLoadingPage(false);
     }).catch((err) => {
+      setLoadingPage(false);
+      if(err.response && err.response.status === 503) err503();
+      else if(err.response && err.response.status === 401) err401();
+      else errGeneral();
     });
-  },[managers.length]); // só re-renderiza se users.length mudar
+  },[pageUpdate]); // só re-renderiza se users.length mudar
 
   function cleanInputs(){
     const form = props.form;
@@ -32,6 +48,11 @@ const UsersWeb = props => {
     form.setFieldsValue({ email: ''});
     form.setFieldsValue({ password: ''});
     form.setFieldsValue({ confirmPassword: ''});
+    form.setFieldsValue({ userNameUpdate: ''});
+    form.setFieldsValue({ emailUpdate: ''});
+    form.setFieldsValue({ passwordUpdate: ''});
+    form.setFieldsValue({ confirmPasswordUpdate: ''});
+
   }
 
   // Modal functions ------------------------------------------------------------------------
@@ -39,7 +60,7 @@ const UsersWeb = props => {
     setModalCadastro(true);
   };
 
-  const handleOk = e => {
+  const handleCadastroOk = e => {
     setConfirmLoading(true);
     props.form.validateFields(['userName', 'email', 'password', 'confirmPassword'], (err, values) => {
       if (!err) {
@@ -47,40 +68,92 @@ const UsersWeb = props => {
         const name = userName;
 
         axios.post('/api/managers/', { email, name, password }).then(() => {
-          notification.success({
-            message: 'Tudo Certo!',
-            description: 'Novo usuário cadastrado!'
-          });
+          success();
           setConfirmLoading(false);
           setModalCadastro(false);
           cleanInputs();
-
+          setPageUpdate(!pageUpdate);
         }).catch(() => {
           setConfirmLoading(false);
           setModalCadastro(false);
-          notification.error({
-            message: 'Opa!',
-            description: 'Houve um problema aqui: Não conseguimos enviar dados no Servidor.'
+          if(err.response && err.response.status === 503) err503();
+          else if(err.response && err.response.status === 401) err401();
+          else errGeneral();
           });
-        });
       } else {
-        notification.error({
-          message: 'Opa!',
-          description: 'Dados inseridos incorretos, Corrija e tente novamente.'
-        });
-        setConfirmLoading(false);
-        setModalCadastro(false);
+          if(err.response && err.response.status === 503) err503();
+          else if(err.response && err.response.status === 401) err401();
+          else errGeneral();
+          setConfirmLoading(false);
+          setModalCadastro(false);
       }
-    }).catch(() => {
-      notification.error({
-        message: 'Opa!',
-        description: 'Probreminha.'
-      });
+    }).catch((err) => {
+      if(err.response && err.response.status === 503) err503();
+      else if(err.response && err.response.status === 401) err401();
+      else errGeneral();
+      setConfirmLoading(false);
+      setModalCadastro(false);
     });
+    setConfirmLoading(false);
+    setModalCadastro(false);
   };
 
-  const handleCancel = e => {
+  const handleCadastroCancel = e => {
     setModalCadastro(false);
+    setConfirmLoading(false);
+  };
+
+  const showUpdateModal = (item) => {
+    setFieldsValue({
+      userNameUpdate: item.name,
+      emailUpdate: item.email,
+    });
+    setItemUpdate(item);
+    setModalUpdate(true);
+  };
+
+  const handleUpdateOk = e => {
+    setConfirmLoading(true);
+    props.form.validateFields(['userNameUpdate', 'emailUpdate', 'passwordUpdate', 'confirmPasswordUpdate'], (err, values) => {
+      if (!err) {
+        const { userNameUpdate, emailUpdate, passwordUpdate, } = values;
+        if(passwordUpdate === ' '){
+          passwordUpdate = itemUpdate.password
+        }
+        axios.put('/api/managers/', { id: itemUpdate._id ,email: emailUpdate, name: userNameUpdate, password: passwordUpdate, superuser: itemUpdate.superuser, image: itemUpdate.image }).then(() => {
+          success();
+          setConfirmLoading(false);
+          setModalUpdate(false);
+          cleanInputs();
+          setPageUpdate(!pageUpdate);
+
+        }).catch((err) => {
+          setConfirmLoading(false);
+          setModalUpdate(false);
+          if(err.response && err.response.status === 503) err503();
+          else if(err.response && err.response.status === 401) err401();
+          else errGeneral();
+          });
+      } else {
+          if(err.response && err.response.status === 503) err503();
+          else if(err.response && err.response.status === 401) err401();
+          else errGeneral();
+          setConfirmLoading(false);
+          setModalUpdate(false);
+      }
+    }).catch((err) => {
+      if(err.response && err.response.status === 503) err503();
+      else if(err.response && err.response.status === 401) err401();
+      else errGeneral();
+      setConfirmLoading(false);
+      setModalUpdate(false);
+    });
+    setConfirmLoading(false);
+    setModalUpdate(false);
+  };
+
+  const handleUpdateCancel = e => {
+    setModalUpdate(false);
     setConfirmLoading(false);
   };
   // End Modal Functions ------------------------------------------------------------------------
@@ -103,6 +176,15 @@ const UsersWeb = props => {
     }
   };
 
+  function compareToFirstPasswordUpdate (rule, value, callback) {
+    const { form } = props;
+    if (value && value !== form.getFieldValue('passwordUpdate')) {
+      callback('senha de confirmação incompativel!');
+    } else {
+      callback();
+    }
+  };
+
   function passwordValidator (rule, value, callback) {
     const { form } = props;
     if (value.length < 6) {
@@ -110,24 +192,122 @@ const UsersWeb = props => {
     }
     callback();
   };
+
+  function passwordValidatorUpdate (rule, value, callback) {
+    const { form } = props;
+    if(value != ''){
+      if (value.length < 6) {
+        callback('digitos minimos: 6');
+      }
+    }
+    callback();
+  };
   // End validators ------------------------------------------------------------------------
 
+  const searchByName = (e) => setManagersFiltered(managers.filter(r => r.name.toLowerCase().includes(e.target.value.toLowerCase())));
+
+  const deleteManager = (id) => {
+    axios.delete(`/api/managers/${id}`).then(res => {
+      success();
+      setPageUpdate(!pageUpdate);
+    }).catch(err => {
+      if(err.response && err.response.status === 503) err503();
+      else if(err.response && err.response.status === 401) err401();
+      else errGeneral();
+    });
+  }
+
+  const changePriority = (item) => {
+    setConfirmLoading(true);
+    console.log("Clicou!");
+    axios.put('/api/managers/', { id: item._id , superuser: !item.superuser }).then(() => {
+      success();
+      setConfirmLoading(false);
+      setPageUpdate(!pageUpdate);
+    }).catch((err) => {
+      setConfirmLoading(false);
+      if(err.response && err.response.status === 503) err503();
+      else if(err.response && err.response.status === 401) err401();
+      else errGeneral();
+      });
+  }
+
   return (
-    <MainLayout page = "web">
-      <Row>
-        <Col span = {4} offset = {20}>
-          <Button
-            type = "primary"
-            style = {{ color: "white"}}
-            onClick = {showCadastroModal}
-          > Adicionar Usuário </Button>
-        </Col>
-      </Row>
+    <MainLayout page = "web" loading = {loadingPage} title = "Gerenciamento de Usuários Web" breadcrumb = {['Gerenciamento', 'Web']}>
+     <Card
+        bordered = {false} className = "alert-card" style = {{ borderRadius: 5 }}
+        title = {
+          <>
+            <Icon type = "container" style = {{ marginRight: 6, color: '#00AD45' }} /> Usuários cadastrados
+          </>
+        }
+        extra = {<Button type = "primary" style = {{ color: "white"}} onClick = {showCadastroModal} > Adicionar Usuário </Button>}
+      >
+        <Row gutter = {24} type = "flex" justify = "end" style = {{ marginBottom: 18 }}>
+          <Col span = {10}>
+            <Search
+              placeholder = "Pesquise por nome"
+              onChange = { e => searchByName(e) }
+              size = "default"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span = {22} type = "flex" justify = "center">
+            <List
+              itemLayout="vertical"
+              size="large"
+              dataSource = { managersFiltered }
+              renderItem = {(item) => (
+                <List.Item
+                  key={item.Name}
+                  actions={[
+                    <Popover content = "editar usuário">
+                      <Icon type="edit" onClick = {() => showUpdateModal(item)} />
+                    </Popover>,
+                    <Popover content = "deletar usuário">
+                      <Icon type="delete"
+                            onClick = { () => {
+                            Modal.confirm({
+                              title: 'Deseja realmente apagar este Usuário?',
+                              content: 'Esta ação é permanente, não haverá forma de restaurar ação.',
+                              okType: 'danger',
+                              onOk() {
+                                deleteManager(item._id)
+                              },
+                              onCancel() {}
+                            });
+                          }}
+                          style = {{ color: "red" }}  />
+                    </Popover>,
+                    <Popover content = "!Superuser">
+                      <Button onCLick ={() => changePriority(item)} > !Superuser </Button>
+                    </Popover>
+                  ]}
+                >
+                  <List.Item.Meta
+                    avatar={
+                      item.superuser ? 
+                      <Badge count={<Popover content={"Esse é um administrador"}> <Icon type = "star" style = {{ color: 'yellow' }}/> </Popover>}>
+                        <Avatar shape="square" size={64} src={item.image} />
+                      </Badge> :
+                      <Avatar shape="square" size={64} src={item.image} />
+                    }
+                    
+                      title={<Text>{item.name}</Text>}
+                    description={item.email}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+        </Row>
+      </Card>
       <Modal
           title="Adicionar Usuário"
           visible={modalCadastro}
-          onOk={handleOk}
-          onCancel={handleCancel}
+          onOk={handleCadastroOk}
+          onCancel={handleCadastroCancel}
           confirmLoading={confirmLoading}
         >
           <Row style = {{ textAlign : "center" }}>
@@ -189,6 +369,66 @@ const UsersWeb = props => {
             </Col>
           </Row> 
         </Modal>
+      <Modal
+        title="Adicionar Usuário"
+        visible={modalUpdate}
+        onOk={handleUpdateOk}
+        onCancel={handleUpdateCancel}
+        confirmLoading={confirmLoading}
+      >
+        <Row style = {{ textAlign : "center" }}>
+          <Col span = {18} offset = {3}>
+            <Form layout = "vertical">
+              <Form.Item>
+                {getFieldDecorator('userNameUpdate', {
+                  // initialValue : ' ',
+                  rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Nome do Usuário"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('emailUpdate', {
+                  // initialValue : ' '  ,
+                  rules: [{type: 'email', message: 'Email Inválido'},
+                    { required: true, message: 'Please input your email!' },
+                  ],
+                })(
+                  <Input
+                    prefix={<Icon type="google" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="E-mail"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('passwordUpdate', {
+                  initialValue : '',
+                  rules: [{validator: passwordValidatorUpdate }],
+                })(
+                  <Input.Password
+                    prefix={<Icon type="unlock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Senha"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('confirmPasswordUpdate', {
+                  initialValue : '',
+                  rules: [{validator: compareToFirstPasswordUpdate }],
+                })(
+                  <Input.Password
+                    prefix={<Icon type="unlock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Confirmação de senha"
+                  />,
+                )}
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row> 
+      </Modal>
     </MainLayout>
   );
 };
