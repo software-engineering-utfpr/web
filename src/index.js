f4ff8a08bc200493d5ae74f7078e9023c655da73
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-10-01 05:02:49 -0300

    Rollback Test

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-axios.defaults.baseURL = 'https://rio-campo-limpo.herokuapp.com/';
-// axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.common = {
-  ...axios.defaults.headers.common,
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
-  "Content-Type": 'application/json'
-};
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
