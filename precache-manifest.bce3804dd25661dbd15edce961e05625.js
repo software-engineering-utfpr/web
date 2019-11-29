de167082f87f458691f278166b26d7f623aab690
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-29 01:13:10 +0000

    Deploy software-engineering-utfpr/web to github.com/software-engineering-utfpr/web.git:gh-pages

---
+++ precache-manifest.bce3804dd25661dbd15edce961e05625.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afee26f566621ed28d86e0e516d447e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95485933a9de42117a42",
+    "url": "/static/css/2.dabab2d0.chunk.css"
+  },
+  {
+    "revision": "67c19a208d836106b9b9",
+    "url": "/static/css/main.2deef87e.chunk.css"
+  },
+  {
+    "revision": "95485933a9de42117a42",
+    "url": "/static/js/2.8d892bcb.chunk.js"
+  },
+  {
+    "revision": "67c19a208d836106b9b9",
+    "url": "/static/js/main.ebe1ce51.chunk.js"
+  },
+  {
+    "revision": "efc5bd544a98f7254c02",
+    "url": "/static/js/runtime~main.27aa8978.js"
+  },
+  {
+    "revision": "0a4c0cca3eb04122704f9eeaa222306e",
+    "url": "/static/media/background.0a4c0cca.jpg"
+  },
+  {
+    "revision": "6ba962ff4e86492ca1fa947444a19f23",
+    "url": "/static/media/logo.6ba962ff.png"
+  },
+  {
+    "revision": "b57ee7cae8fe5f828f62fe8cd3d48d55",
+    "url": "/static/media/right.b57ee7ca.svg"
+  }
+]);
